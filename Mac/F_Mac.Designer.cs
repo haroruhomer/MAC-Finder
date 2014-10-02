696a08849531c5bcf92cadeb4490a6a6235f588a
haroruhomer <harmol22@hotmail.com>
2014-10-01 20:33:26 -0500

    Version 1

---
+++ Mac/F_Mac.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Mac
+{
+    partial class F_Mac
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LB_lista1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // LB_lista1
+            // 
+            this.LB_lista1.FormattingEnabled = true;
+            this.LB_lista1.HorizontalScrollbar = true;
+            this.LB_lista1.Location = new System.Drawing.Point(12, 12);
+            this.LB_lista1.Name = "LB_lista1";
+            this.LB_lista1.ScrollAlwaysVisible = true;
+            this.LB_lista1.Size = new System.Drawing.Size(260, 238);
+            this.LB_lista1.TabIndex = 0;
+            // 
+            // F_Mac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.LB_lista1);
+            this.Name = "F_Mac";
+            this.Text = "MAC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.F_Mac_FormClosing);
+            this.Load += new System.EventHandler(this.F_Mac_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LB_lista1;
+    }
+}
+
---
+++ Mac/F_Mac.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace Mac
+{
+    public partial class F_Mac : Form
+    {
+        private int startIP = 12;
+        private int endIP = 16;
+        private string ipPrefix = "192.168.0";
+        private ArrayList computerList =new ArrayList();// null;
+
+        //public AllPc(string ipPrefix, int startIP, int endIP)
+        //{
+        //    this.startIP = startIP;
+        //    this.endIP = endIP;
+        //    this.ipPrefix = ipPrefix;
+        //    computerList = new ArrayList();
+        //}
+        public F_Mac()
+        {
+            InitializeComponent();
+            ListBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        [DllImport("iphlpapi.dll", ExactSpelling = true)]
+        public static extern int SendARP(int DestIP, int SrcIP, [Out] byte[] pMacAddr, ref int PhyAddrLen);
+
+        public void ScanComputers()
+        {
+            for (int i = startIP; i <= endIP; i++)
+            {
+                Thread.Sleep(10);
+                int r = 0,len=0;
+                string scanIP = ipPrefix + "." + i.ToString();
+                IPAddress myScanIP = IPAddress.Parse(scanIP),ipAddress;
+                IPHostEntry myScanHost = null;
+                IPHostEntry iPHostEntry=null;
+                string[] arr = new string[2];
+                string MAC = null,IP=null;
+
+                try
+                {
+                    myScanHost = Dns.GetHostByAddress(myScanIP);
+                    iPHostEntry = Dns.GetHostEntry(myScanIP);
+                    //IP = iPHostEntry.AddressList[1].ToString();
+                    ipAddress = myScanIP;
+                    byte[] ab = new byte[6];
+                    len = ab.Length;
+                    r = SendARP((int)ipAddress.Address, 0, ab, ref len);
+                    MAC = BitConverter.ToString(ab, 0, 6);
+
+                }
+                catch
+                {
+                    Console.WriteLine(myScanIP);
+                    continue;
+                }
+                if (myScanHost != null)
+                {
+                    arr[0] = myScanHost.HostName;
+                    arr[1] = scanIP;
+                    computerList.Add(arr);
+                    LB_lista1.Items.Add(myScanHost.HostName.ToString() + " IP:" + myScanIP+" MAC: "+MAC);
+                    Console.Write(myScanHost.HostName.ToString() + "\t");
+                    Console.WriteLine(scanIP.ToString());
+                }
+            }
+            LB_lista1.Items.Add("fin");
+        }
+        private void F_Mac_Load(object sender, EventArgs e)
+        {
+            LB_lista1.Items.Add("holi");
+            Thread hilo1 = new Thread(new ThreadStart(ScanComputers));
+            hilo1.Start();
+            //ScanComputers();
+        }
+
+        private void ObtenerDispositivos() 
+        { 
+            
+        }
+
+        private void F_Mac_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
